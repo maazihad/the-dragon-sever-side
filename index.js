939a1738c4840ce713a1ef792e775921bd685261
maazihad <maazihadin@gmail.com>
2023-04-30 14:59:08 +0600

    The dragon serven side half way complete.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+const categories = require("./data/categories.json");
+
+app.get('/', (req, res) => {
+   res.send("Dragon side is running....");
+});
+
+app.use(cors());
+
+app.get('/categories', (req, res) => {
+   res.send(categories);
+});
+
+app.listen(port, () => {
+   console.log(`Dragon API is runnin on port : ${port}`);
+});
