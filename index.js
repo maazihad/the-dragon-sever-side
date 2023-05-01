7aff748e969450282c44e01db9c550cc99ada542
maazihad <maazihadin@gmail.com>
2023-05-01 11:53:05 +0600

    sever site complete.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get('/', (req, res) => {
    res.send("Dragon side is running....");
@@ -14,6 +15,28 @@ app.use(cors());
 
 app.get('/categories', (req, res) => {
    res.send(categories);
+});
+
+app.get('/news', (req, res) => {
+   res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+   const id = req.params.id;
+   const selectedNews = news.find(n => n._id === id);
+   res.send(selectedNews);
+});
+
+
+app.get("/categories/:id", (req, res) => {
+   const id = parseInt(req.params.id);
+   if (id === 0) {
+      res.send(news);
+   }
+   else {
+      const categoriesNews = news.filter(n => parseInt(n.category_id) === id);
+      res.send(categoriesNews);
+   }
 });
 
 app.listen(port, () => {
